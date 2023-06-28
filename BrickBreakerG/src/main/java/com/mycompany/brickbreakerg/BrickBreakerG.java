43a14f568dafdbca05d2054e045e7ad82d0d72e9
RAJA SAHU <117908592+kingstarRaj@users.noreply.github.com>
2023-06-29 04:57:07 +0530

    Add files via upload

---
+++ BrickBreakerG/src/main/java/com/mycompany/brickbreakerg/BrickBreakerG.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.brickbreakerg;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author mr_ra
+ */
+public class BrickBreakerG {
+
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+    
+}
